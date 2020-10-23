78589d15f00ecafed5cdbad91a3e77fa83262809
Isaac Wong <isaac@thirdcaptain.com>
2020-10-23 04:05:30 +0000

    initial commit

--- ch2-getting-started/hello-world/main.tf
+++ ch2-getting-started/hello-world/main.tf
@@ -1,4 +1,4 @@
-#passing in keys via environmental variables
+# passing in keys via environmental variables
 provider "aws" {
 #	access_key	= ""
 #	secret_key	= ""
@@ -6,6 +6,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-	ami		= "ami-0f40c8f97004632f9"
-	instance_type	= "t2.micro"
+	ami						= "ami-0f40c8f97004632f9"
+	instance_type			= "t2.micro"
+	vpc_security_group_ids 	= [aws_security_group.instance.id]
+
+	user_data = <<-EOF
+				#!/bin/bash
+				 echo "Hello, World" > index.html
+				 nohup busybox httpd -f -p 8080 &
+				 EOF
+
+	tags = {
+		Name = "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+		from_port	= 8080
+		to_port		= 8080
+		protocol 	= "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
